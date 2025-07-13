a697ac4e7e0cd88387f249c42f1f360bef0fdf56
Dhruv Kachhawaha <dhruv.kachhawaha2708@gmail.com>
2025-07-13 13:50:44 +0530

    changes

--- poke_fetch/script.js
+++ poke_fetch/script.js
@@ -10,14 +10,18 @@
 
 async function fetchData() {
     try{
-        const pokemonName = document.getElementById("pokemonSpirit")
-         const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
 
             if(!response.ok){
                 throw new Error("cannot fetch the data");
             }
             const data = await response.json();
-            console.log(data);
+            const pokemonSprite = data.sprites.front_default;
+            const imgElement = document.getElementById("pokemonSprite");
+            imgElement.src = pokemonSprite;
+            imgElement.style.display = "block";
+
     }
     catch(error) {
         console.log(error);
