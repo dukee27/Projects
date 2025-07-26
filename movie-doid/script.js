0b3c12de136d7391f651957539fe9d29320a5950
Dhruv Kachhawaha <dhruv.kachhawaha2708@gmail.com>
2025-07-26 15:42:26 +0530

    changes

---
+++ movie-doid/script.js
@@ -0,0 +1,23 @@
+// dom manipulation
+
+//GetElementById()
+
+// const title = document.getElementById('main-heading');
+// console.log(title);
+
+//  GetElementByClassName()
+
+// const listItems = document.getElementsByClassName('list-items');
+// console.log(listItems);
+
+// GetElementsByTagName()
+
+// const listItems = document.getElementsByTagName('li');
+// console.log(listItems);
+
+// query selector()
+
+// const container = document.querySelector('div');
+// console.log(container);
+const container = document.querySelectorAll('div');
+console.log(container);
