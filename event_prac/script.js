c41be66e2ad15ab89a3a57b6ca2f8498db21f394
Dhruv Kachhawaha <dhruv.kachhawaha2708@gmail.com>
2025-07-27 16:27:02 +0530

    changes

--- event_prac/script.js
+++ event_prac/script.js
@@ -13,12 +13,30 @@ function alertBtn(){
 buttonTwo.addEventListener("click",alertBtn);
 
 const change3 = document.querySelector('.boxes-box-3');
+const change2 = document.querySelector('.boxes-box-2');
 
 function changeColor(){
     change3.style.backgroundColor = 'blue';
 }
 
 const buttonThree = document.querySelector('.btn-3');
-const buttonOne = document.querySelector('button');
+const buttonOne = document.querySelector('.btn-1');
 
 buttonThree.addEventListener("mouseover",changeColor);
+
+function reveal(){
+    if(change2.classList.contains('buttonOne')){
+        change2.classList.remove('buttonOne');
+    }
+    else{
+        change2.classList.add('buttonOne');
+    }
+    if(change3.classList.contains('buttonOne')){
+        change3.classList.remove('buttonOne');
+    }
+    else{
+        change3.classList.add('buttonOne');
+    }
+}
+
+buttonOne.addEventListener("click",reveal);
